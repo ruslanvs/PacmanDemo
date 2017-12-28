39001377fe26931b81c7e338955ea38327164b5d
Ruslan <ruslanvs@yahoo.com>
2017-12-27 20:28:51 -0800

    Added turning of the face and appearance and scoring of cherries

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $( document ).ready (function() {
         [2,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
         [2,1,2,1,2,1,2,2,2,1,1,2,1,2,2,2,1,1,2,1,2,2,2,1,1,2,1,2,2,2,1,1,2],
         [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
-        [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
-        [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
+        [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,4,2,1,1,2,1,2,1,2,1,1,2],
+        [2,1,2,4,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
         [2,1,2,2,2,1,2,1,2,1,2,2,1,2,1,2,1,2,2,1,2,1,2,1,2,2,1,2,1,2,1,1,2],
         [2,1,1,1,1,1,2,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,2,1,1,2],
-        [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+        [2,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,2],
         [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]    
     ];
 
@@ -27,6 +27,8 @@ $( document ).ready (function() {
                     output += "<div id = x" + j + "y" + i + " class = 'empty'></div>";
                     if( world[i][j] == 3 )
                     output += "<div id = x" + j + "y" + i + " class = 'pacman'></div>";
+                    if( world[i][j] == 4 )
+                    output += "<div id = x" + j + "y" + i + " class = 'cherry'></div>";
             }
             output += "</div>";
         }
@@ -36,31 +38,56 @@ $( document ).ready (function() {
     displayWorld();
 
     var px = 1, py = 1;
+    var direction = "180deg"
     var pacmanDivId = "";
     var score = 0;
-    document.onkeydown = function(e) {
+    document.onkeydown = function(e) { //setting new coordinates for pacman
+        
+        if ( e.keyCode  == 37) {
+            direction = "0deg"; //turning pacman's face
+            if ( world[py][px-1] !== 2 ) {
+                px --;
+            }
+        }
+        if ( e.keyCode  == 39) {
+            direction = "180deg";
+            if ( world[py][px+1] !== 2  ) {
+                px ++;
+            } 
+        }
+        if ( e.keyCode  == 38) {
+            direction = "90deg";
+            if ( world[py-1][px] !== 2  ) {
+                py --;
+            } 
+        }
+        if ( e.keyCode  == 40) {
+            direction = "270deg";
+            if ( world[py+1][px] !== 2  ) {
+                py ++;
+            } 
+        }
+        
+        $( "div.pacman" ).attr( "class", "empty" ); //removing pacman from old coordinates
+        
+        pacmanDivId = "#x" + px + "y" + py + ""; //building up the id with new coordinates for pacman's div
 
-        if ( e.keyCode  == 37 && world[py][px-1] !== 2 ) {
-            px --;
-        }
-        if ( e.keyCode  == 39 && world[py][px+1] !== 2  ) {
-            px ++;
-        }
-        if ( e.keyCode  == 38 && world[py-1][px] !== 2  ) {
-            py --;
-        }
-        if ( e.keyCode  == 40 && world[py+1][px] !== 2  ) {
-            py ++;
-        }
-        
-        $( "div.pacman" ).attr( "class", "empty" );
-        
-        pacmanDivId = "#x" + px + "y" + py + "";
         if ($( pacmanDivId ).attr( "class" ) == "coin") {
             score += 10;
-            document.getElementById("score").innerHTML = "<p>SCORE: " + score + "</p>";
         }
-        $( pacmanDivId ).attr( "class", "pacman" );
+        if ($( pacmanDivId ).attr( "class" ) == "cherry") {
+            score += 50;
+            $( "body" ).css( 'background-color', 'red');
+            setTimeout (function () {
+                $( "body" ).css( 'background-color', 'black');
+            }, 200)
+        }
+
+        $( pacmanDivId ).attr( "class", "pacman" ); //creating pacman at the new coordinates
+        $( "div.pacman" ).css( 'transform', 'rotate(' + direction + ')' );
+
+        document.getElementById("score").innerHTML = "<p>SCORE: " + score + "</p>";
+
 
     }
 
