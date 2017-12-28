16b7110c8e3f5a35e51499cef1d23874d1136d12
Ruslan <ruslanvs@yahoo.com>
2017-12-27 19:02:22 -0800

    Assignment complete

---
+++ script.js
@@ -0,0 +1,67 @@
+$( document ).ready (function() {
+    var world = [
+        [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [2,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+        [2,1,2,1,2,1,2,2,2,1,1,2,1,2,2,2,1,1,2,1,2,2,2,1,1,2,1,2,2,2,1,1,2],
+        [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
+        [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
+        [2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2,1,2,1,2,1,1,2],
+        [2,1,2,2,2,1,2,1,2,1,2,2,1,2,1,2,1,2,2,1,2,1,2,1,2,2,1,2,1,2,1,1,2],
+        [2,1,1,1,1,1,2,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,2,1,1,2],
+        [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+        [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]    
+    ];
+
+    function displayWorld() {
+        var output = "";
+
+        for( var i = 0; i<world.length; i++) {
+            output += "<div class = 'row'>";
+            
+            for( var j = 0; j<world[i].length; j++) {
+                if( world[i][j] == 2 )
+                    output += "<div id = x" + j + "y" + i + " class = 'brick'></div>";
+                    else if( world[i][j] == 1 )
+                    output += "<div id = x" + j + "y" + i + " class = 'coin'></div>";
+                    if( world[i][j] == 0 )
+                    output += "<div id = x" + j + "y" + i + " class = 'empty'></div>";
+                    if( world[i][j] == 3 )
+                    output += "<div id = x" + j + "y" + i + " class = 'pacman'></div>";
+            }
+            output += "</div>";
+        }
+        output += "<p id = score>SCORE: 0</p>"
+        document.getElementById("world").innerHTML = output;
+    }
+    displayWorld();
+
+    var px = 1, py = 1;
+    var pacmanDivId = "";
+    var score = 0;
+    document.onkeydown = function(e) {
+
+        if ( e.keyCode  == 37 && world[py][px-1] !== 2 ) {
+            px --;
+        }
+        if ( e.keyCode  == 39 && world[py][px+1] !== 2  ) {
+            px ++;
+        }
+        if ( e.keyCode  == 38 && world[py-1][px] !== 2  ) {
+            py --;
+        }
+        if ( e.keyCode  == 40 && world[py+1][px] !== 2  ) {
+            py ++;
+        }
+        
+        $( "div.pacman" ).attr( "class", "empty" );
+        
+        pacmanDivId = "#x" + px + "y" + py + "";
+        if ($( pacmanDivId ).attr( "class" ) == "coin") {
+            score += 10;
+            document.getElementById("score").innerHTML = "<p>SCORE: " + score + "</p>";
+        }
+        $( pacmanDivId ).attr( "class", "pacman" );
+
+    }
+
+})
